32fa54f9359b81a5bb0d7b9c70783b2d5b859b35
Marco Bacci <marcobacci94@gmail.com>
2025-07-01 15:12:39 +0200

    three const determined

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// età utente e km da percorrere
+const UserAge = parseFloat(prompt ('inserisci età'))
+const NumKm = parseFloat(prompt ('inserisci numero km'))
+const TicketPrice = NumKm * 0.21
+
+
